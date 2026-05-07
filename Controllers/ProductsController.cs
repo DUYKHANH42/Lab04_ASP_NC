5a786bb344628767b9200499eb2bce28d7c38376
DUYKHANH42 <kxtduykhanh@gmail.com>
2026-05-07 16:15:04 +0700

    lab05

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Lab04.Models;
+using Lab04.VM;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,10 +16,29 @@ namespace Lab04.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string keyword = "", int page = 1)
         {
-            var lstProduct = _context.Products.Include(p => p.Category).ToList();
-            return View(lstProduct);
+            int pageSize = 5;
+
+            var query = _context.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query = query.Where(x => x.Name.Contains(keyword));
+            }
+
+            int totalProducts = query.Count();
+            var products = query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+            ViewBag.Keyword = keyword;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return PartialView("_ProductTable", products);
+            }
+            return View(products);
         }
         public IActionResult Create()
         {
@@ -83,6 +103,25 @@ namespace Lab04.Controllers
             _context.Products.Remove(product);
             _context.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public IActionResult ThongKe()
+        {
+            var result = _context.Products
+                     .Include(p => p.Category)
+                     .AsEnumerable()
+                     .GroupBy(p => new { p.CategoryId, p.Category.Name })
+                     .Select(g => new ThongKeVM
+                     {
+                         TenDanhMuc = g.Key.Name,
+                         SoLuongSanPham = g.Count(),
+                         GiaLonNhat = g.Max(p => p.Price),
+                         GiaNhoNhat = g.Min(p => p.Price),
+                         GiaTrungBinh = g.Average(p => p.Price),
+                         TongGiaTri = g.Sum(p => p.Price)
+                     })
+                     .ToList();
+
+            return View(result);
         }
     }
 }
---
+++ VM/ThongKeVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab04.VM
+{
+    public class ThongKeVM
+    {
+        public string TenDanhMuc { get; set; }
+        public int SoLuongSanPham { get; set; }
+        public decimal GiaLonNhat { get; set; }
+        public decimal GiaNhoNhat { get; set; }
+        public decimal GiaTrungBinh { get; set; }
+        public decimal TongGiaTri { get; set; }
+    }
+}
